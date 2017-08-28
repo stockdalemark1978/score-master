29b0223355cc5013a4abe588d92f8ead217d5678
stockdalemark1978 <stockdalemark1978@gmail.com>
2017-08-28 14:44:40 -0400

    adding in players variable and output

--- client/main.js
+++ client/main.js
@@ -2,6 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Meteor} from 'meteor/meteor';
 
+const players = [{
+  _id: '1',
+  name: 'Mark',
+  score: -15
+}, {
+  _id: '2',
+  name: 'Lindsey',
+  score: 80
+}, {
+  _id: '3',
+  name: 'Cashie',
+  score: 150
+}];
+
 Meteor.startup(function (){
     let title = 'Score Master';
     let name = 'Mark';
@@ -10,6 +24,7 @@ Meteor.startup(function (){
             <h1>{title}</h1>
             <p>Hi {name}!</p>
             <p>This is my second p.</p>
+            {[<p>1</p>, <p>2</p>]}
         </div>
     );
     ReactDOM.render(jsx, document.getElementById('app'));
