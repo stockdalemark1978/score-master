b49fb9534412a751dcb31fd3606a437bdd56cb12
stockdalemark1978 <stockdalemark1978@gmail.com>
2017-08-28 16:48:49 -0400

    adding in JSX to render players and scores

--- client/main.js
+++ client/main.js
@@ -16,6 +16,23 @@ const players = [{
   score: 150
 }];
 
+const renderPlayers = function (players){
+
+    // let numbers = [{val: }, {val: 2}, {val: 3}];
+    //
+    // return numbers.map(function(number){
+    //   return <p key={number.val}>{number.val}</p>;
+    // });
+
+    return players.map(function(player){
+      return (
+        <div key={player._id}>
+          <p >{player.name} has {player.score} point(s)</p>
+        </div>);
+    });
+};
+
+
 Meteor.startup(function (){
     let title = 'Score Master';
     let name = 'Mark';
@@ -24,7 +41,7 @@ Meteor.startup(function (){
             <h1>{title}</h1>
             <p>Hi {name}!</p>
             <p>This is my second p.</p>
-            {[<p>1</p>, <p>2</p>]}
+            {renderPlayers(players)}
         </div>
     );
     ReactDOM.render(jsx, document.getElementById('app'));
