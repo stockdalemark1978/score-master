c0cc28d90fb05921d223dd5fbef56251fbf34be4
stockdalemark1978 <stockdalemark1978@gmail.com>
2017-08-27 21:41:25 -0400

    imported react libraries and added some JSX code

--- client/main.js
+++ client/main.js
@@ -1,4 +1,9 @@
-import Default from './../imports/utils';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Meteor} from 'meteor/meteor';
 
-console.log('hello from /client/main.js')
-console.log(Default);
+Meteor.startup(function (){
+    let name = 'Mark';
+    let jsx = <p>Hi {name}!</p>;
+    ReactDOM.render(jsx, document.getElementById('app'));
+});
