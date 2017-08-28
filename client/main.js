3d08993dbd0e6df65e530ae7aba8898b5e05096a
stockdalemark1978 <stockdalemark1978@gmail.com>
2017-08-28 14:37:43 -0400

    adding nested html items using react

--- client/main.js
+++ client/main.js
@@ -3,7 +3,14 @@ import ReactDOM from 'react-dom';
 import {Meteor} from 'meteor/meteor';
 
 Meteor.startup(function (){
+    let title = 'Score Master';
     let name = 'Mark';
-    let jsx = <p>Hi {name}!</p>;
+    let jsx = (
+        <div>
+            <h1>{title}</h1>
+            <p>Hi {name}!</p>
+            <p>This is my second p.</p>
+        </div>
+    );
     ReactDOM.render(jsx, document.getElementById('app'));
 });
