5b681ff74ed9acb1a50429e38a0160e3d2aff1b7
stockdalemark1978 <stockdalemark1978@gmail.com>
2017-08-28 17:46:53 -0400

    seeding Mongo DB

---
+++ server/main.js
@@ -0,0 +1,10 @@
+import {Meteor} from 'meteor/meteor';
+import {Players} from './../imports/api/players';
+
+Meteor.startup(function (){
+  Players.insert({
+    name: 'Lindsey',
+    score: 20
+  });
+  console.log(Players.find().fetch());
+});
