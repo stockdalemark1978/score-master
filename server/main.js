a0f77e25cf731553a4f32fd4dd8a6c891ed49b83
stockdalemark1978 <stockdalemark1978@gmail.com>
2017-08-29 18:54:02 -0400

    incorporating arrow ES6 arrow functions in main.js server

--- server/main.js
+++ server/main.js
@@ -6,5 +6,20 @@ Meteor.startup(function (){
   //   name: 'Lindsey',
   //   score: 20
   // });
-  console.log(Players.find().fetch());
+  // console.log(Players.find().fetch());
+
+  // let square = function (x) {
+  //   return x * x;
+  // };
+
+  // let square = (x) => x * x;
+
+  // console.log (square(10));
+
+  let numbers = [9, 99, 4, 56];
+
+  let newNumbers = numbers.map((number) => number + 1);
+
+console.log(newNumbers);
+
 });
