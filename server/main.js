a3c369e79a9e2a627d65cd66c4e5cdd4c67211d8
stockdalemark1978 <stockdalemark1978@gmail.com>
2017-09-01 14:24:37 -0400

    playing with ES6 classes

--- server/main.js
+++ server/main.js
@@ -2,6 +2,12 @@ import {Meteor} from 'meteor/meteor';
 import {Players} from './../imports/api/players';
 
 Meteor.startup(() => {
+  class Person {
+    constructor(name) {
+      this.name = name;
+    }
+  }
 
-
+  let me = new Person('Mark');
+  console.log(me);
 });
