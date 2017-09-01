927be7512c2f272f203cde61982a7b4ad7232fea
stockdalemark1978 <stockdalemark1978@gmail.com>
2017-09-01 14:41:11 -0400

    added ES6 templating in methods for classes

--- server/main.js
+++ server/main.js
@@ -3,14 +3,22 @@ import {Players} from './../imports/api/players';
 
 Meteor.startup(() => {
   class Person {
-    constructor(name = 'Anonymous') {
+    constructor(name = 'Anonymous', age = 0) {
       this.name = name;
+      this.age = age;
     }
     getGreeting() {
       return `Hello! My name is ${this.name}.`;
     }
+    getPersonDescription() {
+      return `${this.name} is ${this.age} year(s) old!`;
+    }
   }
 
   let me = new Person('Mark');
+
+  let somebodyElse = new Person('Lindsey', 35);
   console.log(me.getGreeting());
+  console.log(me.getPersonDescription());
+  console.log(somebodyElse.getPersonDescription());
 });
