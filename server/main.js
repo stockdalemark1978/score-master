62a34ff53d31cdbbc4f71e5a214e1a1f3a7e5c55
stockdalemark1978 <stockdalemark1978@gmail.com>
2017-09-01 20:28:37 -0400

    working on extending Classes

--- server/main.js
+++ server/main.js
@@ -15,10 +15,29 @@ Meteor.startup(() => {
     }
   }
 
-  let me = new Person('Mark');
+  class Employee extends Person {
+    constructor(name, age, title) {
+      super(name, age);
+      this.title = title;
+    }
+    hasJob() {
+      return !!this.title;
+    }
+    getGreeting() {
+      if (this.title) {
+        return `Hi, My name is ${this.name} and I work as ${this.title}.`;
+      } else {
+        return super.getGreeting();
+      }
+    }
+  }
 
-  let somebodyElse = new Person('Lindsey', 35);
-  console.log(me.getGreeting());
+  let me = new Employee('Mark', 38, 'db admin');
   console.log(me.getPersonDescription());
+  console.log(me.getGreeting());
+
+  let somebodyElse = new Employee('Lindsey', 35);
+
   console.log(somebodyElse.getPersonDescription());
+  console.log(somebodyElse.getGreeting());
 });
