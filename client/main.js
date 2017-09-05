400b11d9b7b0eebdf84b29a22b07892d321a73ad
stockdalemark1978 <stockdalemark1978@gmail.com>
2017-09-05 18:22:30 -0400

    added Player component

--- client/main.js
+++ client/main.js
@@ -5,23 +5,14 @@ import {Tracker} from 'meteor/tracker';
 import {Players} from './../imports/api/players';
 import TitleBar from './../imports/ui/TitleBar';
 import AddPlayer from './../imports/ui/AddPlayer';
+import Player from './../imports/ui/Player';
 
 
 
 const renderPlayers = (players) => {
     return players.map((player) => {
-      return (
-        <p key={player._id}>
-          {player.name} has {player.score} point(s).
-          <button onClick={() => {
-            Players.update(player._id, {$inc:{score: 1}});
-          }}>+1</button>
-          <button onClick={() => {
-            Players.update(player._id, {$inc:{score: -1}});
-          }}>-1</button>
-          <button onClick={() => Players.remove(player._id)}>X</button>
-        </p>
-      );
+      return <Player key={player._id} />;
+
     });
 };
 
