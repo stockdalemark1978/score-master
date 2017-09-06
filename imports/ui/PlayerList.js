db99811887cbabb33e334198cccde80d8429a628
stockdalemark1978 <stockdalemark1978@gmail.com>
2017-09-06 17:26:13 -0400

    added if statement for no players added

--- imports/ui/PlayerList.js
+++ imports/ui/PlayerList.js
@@ -4,10 +4,17 @@ import Player from './Player';
 
 export default class PlayerList extends React.Component {
   renderPlayers() {
-    return this.props.players.map((player) => {
-      return <Player key={player._id} player={player}/>;
-
-    });
+    if (this.props.players.length === 0) {
+      return (
+        <div>
+          <p>No players have been added. Please add your first player.</p>
+        </div>
+      );
+    }else {
+      return this.props.players.map((player) => {
+        return <Player key={player._id} player={player}/>;
+      });
+    }
   }
   render() {
     return (
