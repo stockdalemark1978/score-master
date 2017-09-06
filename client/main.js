eee2c2d1c156984bb79c10ced0c57ce0aa1fc56d
stockdalemark1978 <stockdalemark1978@gmail.com>
2017-09-06 18:16:22 -0400

    added main App component and rearranged code

--- client/main.js
+++ client/main.js
@@ -2,27 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Meteor} from 'meteor/meteor';
 import {Tracker} from 'meteor/tracker';
-import {Players} from './../imports/api/players';
-import TitleBar from './../imports/ui/TitleBar';
-import AddPlayer from './../imports/ui/AddPlayer';
-import PlayerList from './../imports/ui/PlayerList';
 
+import {Players} from './../imports/api/players';
+import App from './../imports/ui/App';
 
 Meteor.startup(() => {
   Tracker.autorun(() => {
     let players = Players.find().fetch();
-    let title = 'Score Master';
-    let subTitle = 'Created By Mark Stockdale';
-    let jsx = (
-        <div>
-            <TitleBar title={title} subTitle={subTitle}/>
-            <PlayerList players={players}/>
-            <AddPlayer/>
-
-        </div>
-    );
-    ReactDOM.render(jsx, document.getElementById('app'));
+    let title = 'Score Keep';
+    ReactDOM.render(<App title={title} players={players}/>, document.getElementById('app'));
   });
-
-
 });
