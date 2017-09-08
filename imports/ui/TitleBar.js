a08ce3d390a2acef6325c9978c3551e05d3ec5b2
stockdalemark1978 <stockdalemark1978@gmail.com>
2017-09-08 14:02:36 -0400

    adding additional css

--- imports/ui/TitleBar.js
+++ imports/ui/TitleBar.js
@@ -11,8 +11,10 @@ export default class TitleBar extends React.Component{
   render() {
     return (
       <div className="title-bar">
-        <h1>{this.props.title}</h1>
-        {this.renderSubtitle()}
+        <div className="wrapper">
+          <h1>{this.props.title}</h1>
+          {this.renderSubtitle()}
+        </div>
       </div>
     );
   }
