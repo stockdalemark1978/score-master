9ea4554905aca05b7a9164c5d0f0a181137b9401
stockdalemark1978 <stockdalemark1978@gmail.com>
2017-09-17 12:29:01 -0400

    adding styling to player name and score info

--- imports/ui/Player.js
+++ imports/ui/Player.js
@@ -6,14 +6,17 @@ export default class Player extends React.Component {
   render() {
     return (
         <div className="item" key={this.props.player._id}>
-          <p>{this.props.player.name} has {this.props.player.score} point(s).</p>
-          <button className="button button--round" onClick={() => {
-            Players.update(this.props.player._id, {$inc:{score: 1}});
-          }}>+1</button>
-          <button className="button button--round" onClick={() => {
-            Players.update(this.props.player._id, {$inc:{score: -1}});
-          }}>-1</button>
-          <button className="button button--round" onClick={() => Players.remove(this.props.player._id)}>X</button>
+          <div className="player">
+            <h3 className="player__name">{this.props.player.name}</h3>
+            <p className="player__stats">{this.props.player.score} point(s).</p>
+            <button className="button button--round" onClick={() => {
+              Players.update(this.props.player._id, {$inc:{score: 1}});
+            }}>+1</button>
+            <button className="button button--round" onClick={() => {
+              Players.update(this.props.player._id, {$inc:{score: -1}});
+            }}>-1</button>
+            <button className="button button--round" onClick={() => Players.remove(this.props.player._id)}>X</button>
+          </div>
         </div>
     );
   }
