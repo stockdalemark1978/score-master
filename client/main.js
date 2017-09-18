4475b1034b4e8c6c9dbccc77fa2d273b73adf27b
stockdalemark1978 <stockdalemark1978@gmail.com>
2017-09-18 13:36:49 -0400

    added more player rank functions

--- client/main.js
+++ client/main.js
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom';
 import {Meteor} from 'meteor/meteor';
 import {Tracker} from 'meteor/tracker';
 
-import {Players} from './../imports/api/players';
+import {Players, calcPlayerPositions} from './../imports/api/players';
 import App from './../imports/ui/App';
 
 Meteor.startup(() => {
   Tracker.autorun(() => {
     let players = Players.find({}, {sort: {score: -1}}).fetch();
+    let positionPlayers = calcPlayerPositions(players);
     let title = 'Score Master';
-    ReactDOM.render(<App title={title} players={players}/>, document.getElementById('app'));
+    ReactDOM.render(<App title={title} players={positionPlayers}/>, document.getElementById('app'));
   });
 });
