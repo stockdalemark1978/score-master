8418802b4bdd3aed7566a94284789557036b9280
stockdalemark1978 <stockdalemark1978@gmail.com>
2017-09-18 19:00:50 -0400

    added dynamic color for positions

--- imports/ui/Player.js
+++ imports/ui/Player.js
@@ -4,13 +4,15 @@ import {Players} from './../api/players';
 export default class Player extends React.Component {
 
   render() {
+
+    let itemClassName = `item item--position-${this.props.player.rank}`
     return (
-        <div className="item" key={this.props.player._id}>
+        <div className={itemClassName} key={this.props.player._id}>
           <div className="player">
             <div>
               <h3 className="player__name">{this.props.player.name}</h3>
               <p className="player__stats">
-                {this.props.player.rank} {this.props.player.position} {this.props.player.score} point(s).</p>
+                {this.props.player.position} place - {this.props.player.score} point(s).</p>
             </div>
             <div className="player__actions">
                 <button className="button button--round" onClick={() => {
